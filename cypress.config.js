b9ae83bfd1f05f587a2675f9bf13bad01c84b8bc
Mateus Manganelli <mateusmanganelli99@gmail.com>
2026-04-24 15:25:51 -0300

    pipeline com api + ui

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,9 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://automationexercise.com',
-    defaultCommandTimeout: 10000
-  },
-  retries: 2
-});
-
 module.exports = {
   e2e: {
-    video: true
+    baseUrl: 'https://automationexercise.com',
+    video: true,
+    screenshotOnRunFailure: true
   }
 }
